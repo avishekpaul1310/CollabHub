93e1418efa003f84c7f28e4c903bf2e7cae3489b
Avishekpaul1310 <avishekpaul1310@gmail.com>
2025-03-28 18:47:03 +0530

    Add thread detail and form templates, and update routing for threaded messaging

--- workspace/routing.py
+++ workspace/routing.py
@@ -1,7 +1,8 @@
 from django.urls import re_path
-from workspace.consumers import ChatConsumer, NotificationConsumer
+from workspace.consumers import ChatConsumer, NotificationConsumer, ThreadConsumer
 
 websocket_urlpatterns = [
     re_path(r'ws/chat/(?P<work_item_id>\w+)/$', ChatConsumer.as_asgi()),
+    re_path(r'ws/thread/(?P<thread_id>\w+)/$', ThreadConsumer.as_asgi()),
     re_path(r'ws/notifications/$', NotificationConsumer.as_asgi()),
 ]
