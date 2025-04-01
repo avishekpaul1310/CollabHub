5e6ecf00f9f4a3de6b7079a48a6e7cf49b766289
Avishekpaul1310 <avishekpaul1310@gmail.com>
2025-04-01 21:01:23 +0530

    Update TIME_ZONE setting to 'Asia/Kolkata' for regional time configuration

--- collabhub/settings.py
+++ collabhub/settings.py
@@ -119,12 +119,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
