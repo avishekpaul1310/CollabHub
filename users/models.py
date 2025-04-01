b8a1f024becb5571a12b433616ea5d9e62806be4
Avishekpaul1310 <avishekpaul1310@gmail.com>
2025-04-02 00:05:47 +0530

    Add OnlineStatus model to track user activity and status

--- users/models.py
+++ users/models.py
@@ -8,3 +8,12 @@ class Profile(models.Model):
     
     def __str__(self):
         return f'{self.user.username} Profile'
+    
+class OnlineStatus(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='online_status')
+    status = models.CharField(max_length=20, default='offline')
+    status_message = models.CharField(max_length=255, blank=True, null=True)
+    last_activity = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"{self.user.username}: {self.status}"
